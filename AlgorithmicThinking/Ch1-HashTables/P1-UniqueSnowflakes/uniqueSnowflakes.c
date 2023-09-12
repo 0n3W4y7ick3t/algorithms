056cd796b0f04a679556a68c21cee6ab05c77dec
leon <leonsatoshi39@gmail.com>
2023-09-12 19:18:54 +0900

    Ch1-P2

--- AlgorithmicThinking/Ch1-HashTables/P1-UniqueSnowflakes/uniqueSnowflakes.c
+++ AlgorithmicThinking/Ch1-HashTables/P1-UniqueSnowflakes/uniqueSnowflakes.c
@@ -5,15 +5,100 @@
  */
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 
-void solve() {
-  //
+#define WINGS_OF_SNOWFLAKE 6
+#define MAX_NUM_OF_SNOWFLAKE 100000
+
+typedef struct snowflake_node {
+  int snow_flake[WINGS_OF_SNOWFLAKE];
+  struct snowflake_node* next;
+}snowflake_node;
+
+int encode_snow(snowflake_node* node) {
+  int sum = 0;
+  int i;
+  for(i = 0; i < WINGS_OF_SNOWFLAKE; i++){
+    sum+= node->snow_flake[i];
+  }
+  return sum % MAX_NUM_OF_SNOWFLAKE;
+}
+
+int identical_backwards(int snow1[], int snow2[], int offset) {
+  int i;
+  for (i = 0; i < WINGS_OF_SNOWFLAKE; i++){
+    /* comapre backwards */
+    if (snow1[i] != snow2[(WINGS_OF_SNOWFLAKE - i + offset) % WINGS_OF_SNOWFLAKE]) {
+      return 0;
+      break;
+    }
+  }
+  return 1;
+}
+
+int identical_forwards(int snow1[], int snow2[], int offset) {
+  int i;
+  for (i = 0; i < WINGS_OF_SNOWFLAKE; i++){
+    /* comapre forwards */
+    if (snow1[i] != snow2[(i + offset) % WINGS_OF_SNOWFLAKE]) {
+      return 0;
+      break;
+    }
+  }
+  return 1;
+}
+
+int are_identical(int snow1[], int snow2[]) {
+  int offset;
+  for (offset = 0; offset < WINGS_OF_SNOWFLAKE; offset++) {
+    if (identical_backwards(snow1, snow2, offset))
+      return 1;
+    if (identical_forwards(snow1, snow2, offset))
+      return 1;
+  }
+  return 0;
+}
+
+void solve(snowflake_node *snows[]) {
+  snowflake_node *snow1, *snow2;
+  for (int i=0; i < MAX_NUM_OF_SNOWFLAKE; i++) {
+    /* compare all snows in the same link list */
+    snow1 = snows[i];
+    while(snow1 != NULL) {
+      snow2 = snow1 -> next;
+      while(snow2 != NULL) {
+        if (are_identical(snow1->snow_flake, snow2-> snow_flake)) {
+          printf("Twin snowflakes found.\n");
+          return;
+        }
+        snow2 = snow2->next;
+      }
+      snow1 = snow1->next;
+    }
+  }
+  printf("No two snowflakes are alike.\n");
 }
 
 int main(void) {
-  // dealing inputs
-  solve();
+  int n, code;
+  scanf("%d", &n);
+  static snowflake_node *snowflakes[MAX_NUM_OF_SNOWFLAKE] = {NULL};
+  snowflake_node *snow;
+
+  for (int i=0; i < n; i++) {
+    snow = malloc(sizeof(snowflake_node));
+    if(snow == NULL) {
+      fprintf(stderr, "malloc err!\n");
+      exit(1);
+    }
+    for (int k=0; k < WINGS_OF_SNOWFLAKE; k++)
+      scanf("%d", &snow->snow_flake[k]);
+    /* store this snow to array */
+    code = encode_snow(snow);
+    snow->next = snowflakes[code];
+    snowflakes[code] = snow;
+  }
+
+  solve(snowflakes);
   return 0;
 }
 
---
+++ AlgorithmicThinking/Ch1-HashTables/P2-CompoundWords/compoundWords.c
@@ -0,0 +1,131 @@
+/**
+ * @author leon
+ * @created Sep 12, 2023 at 14:43:59
+ * @link https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=15&page=show_problem&problem=1332
+ * UVa problem 10391
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define NUM_BITS 17 /* bits size of hash */
+#define WORD_LENGTH 16
+#define hashsize(n) ((unsigned long)1 << n)
+#define hashmask(n) (hashsize(n) - 1) /* 100 -> 011 */
+
+typedef struct word_node {
+  char **word;
+  struct word_node *next;
+} word_node;
+
+unsigned long oaat(char* key, unsigned long len, unsigned long bits) {
+  /* the hash function */
+  unsigned long hash, i;
+  for (hash = 0, i = 0; i < len; i++) {
+    hash += key[i];
+    hash += (hash << 10);
+    hash ^= (hash >> 6);
+  }
+
+  hash += (hash << 3);
+  hash ^= (hash >> 11);
+  hash += (hash << 15);
+  return hash & hashmask(bits);
+}
+
+int in_hash_table(word_node *hash_table[], char *find, unsigned find_len) {
+  unsigned word_hash;
+  word_node *wordptr;
+  word_hash = oaat(find, find_len, NUM_BITS);
+  wordptr = hash_table[word_hash];
+  while(wordptr) {
+    if ( strlen(*(wordptr->word)) == find_len &&
+        strncmp(*(wordptr->word), find, find_len) == 0)
+      return 1;
+    wordptr = wordptr->next;
+  }
+  /* when wordptr is null leaves while loop */
+  return 0;
+}
+
+char* read_line(int size) {
+  char *str;
+  int ch;
+  int len = 0;
+  str = malloc(size);
+  if (str == NULL) {
+    fprintf(stderr, "malloc error\n");
+    exit(1);
+  }
+  while( (ch = getchar()) != EOF && (ch != '\n') ) {
+    str[len++] = ch;
+    /* len inc, check if size is too small */
+    if(len == size) {
+      size = size * 2;
+      str = realloc(str, size);
+      if (str == NULL) {
+        fprintf(stderr, "malloc error\n");
+        exit(1);
+      }
+    }
+  }
+  str[len] = '\0';
+  return str;
+}
+
+void solve(char *words[], word_node *hash_table[], int total_words) {
+  int i, j;
+  for (i=0; i < total_words; i++) {
+    /* split every word, see if both parts can be found in hash_table */
+    int len = strlen(words[i]);
+    for (j=0; j < len; j++) {
+      /* by passing 3rd param to in_hash_table(), we are limit the range of cstring to be check on
+         start at words[i], for a length of j
+         start ar words[i][j], for a length of (len - j) */
+      if (in_hash_table(hash_table, words[i], j) &&
+          in_hash_table(hash_table, &words[i][j], len -j )) {
+        /* both found */
+        printf("%s\n", words[i]);
+        break; /* no need to check words[i] for the rest splits */
+      }
+    }
+  }
+}
+
+int main(void) {
+  static char *words[ 1 << NUM_BITS ] = {NULL};
+  static word_node *hash_table[1 << NUM_BITS] = {NULL};
+  int total_words = 0;
+  char *word;
+  word_node *wordptr;
+  unsigned length, word_hash;
+
+  word = read_line(WORD_LENGTH);
+  while (*word) {
+    /* if read a word */
+    words[total_words] = word;
+    wordptr = malloc(sizeof(word_node));
+    if (wordptr == NULL) {
+      fprintf(stderr, "malloc error\n");
+      exit(1);
+    }
+    length = strlen(word);
+    word_hash = oaat(word, length, NUM_BITS);
+    wordptr->word = &words[total_words];
+    /* TODO: if change last line to
+       wordptr->word = &word;
+       why this failed to store word in hash_table
+       table[somehash] is a one node link list,
+       whose `word` is empty.
+      */
+    wordptr->next = hash_table[word_hash];
+    hash_table[word_hash] = wordptr;
+    total_words++;
+
+    word = read_line(WORD_LENGTH); /* read next one */
+  }
+
+  solve(words, hash_table, total_words);
+  return 0;
+}
+
