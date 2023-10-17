4ccdc31598f6775a78c59e7df79124df787df33f
leon <leonsatoshi39@gmail.com>
2023-10-17 16:51:16 +0900

    bitmap snippet

---
+++ AlgorithmicThinking/snippets/bitmap.c
@@ -0,0 +1,17 @@
+#include <limits.h>
+#include <stdint.h>
+#include <string.h>
+
+typedef uint32_t word_t;
+enum { BIT_PER_WORD = sizeof(word_t) * CHAR_BIT };
+#define WORD_OFFSET(b) ((b) / BIT_PER_WORD)
+#define BIT_OFFSET(b) ((b) % BIT_PER_WORD)
+
+void set_bit(word_t *w, int n) { w[WORD_OFFSET(n)] |= (1 << BIT_OFFSET(n)); }
+
+void clear_bit(word_t *w, int n) { w[WORD_OFFSET(n)] &= ~(1 << BIT_OFFSET(n)); }
+
+int get_bit(word_t *w, int n) {
+  word_t bit = w[WORD_OFFSET(n)] & (1 << BIT_OFFSET(n));
+  return bit != 0;
+}
