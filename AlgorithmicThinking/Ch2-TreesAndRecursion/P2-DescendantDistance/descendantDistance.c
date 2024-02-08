b1676fc446915160588bc40136cd7daf29d595ea
leon <leonsatoshi39@gmail.com>
2024-02-08 11:15:11 +0900

    move on.. cant get right anwser on oj

--- AlgorithmicThinking/Ch2-TreesAndRecursion/P2-DescendantDistance/descendantDistance.c
+++ AlgorithmicThinking/Ch2-TreesAndRecursion/P2-DescendantDistance/descendantDistance.c
@@ -2,6 +2,9 @@
  * @author leon
  * @date Sep 21, 2023 at 11:09:45
  * @link https://dmoj.ca/problem/ecna05b
+ * !!! cant get right anwser on oj !!!
+ * diffrent output with local one (same test case)
+ * cant figure out whats wrong
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +27,8 @@ typedef struct node {
 void *safe_malloc(int size) {
   char *mem = malloc(size);
   if (!mem) {
-    fprintf(stderr, "malloc error\n");
+    fprintf(stderr, 
+    "malloc error\n");
     exit(1);
   }
   return mem;
@@ -53,7 +57,8 @@ unsigned long oaat(char *key, unsigned long len, unsigned long bits) {
   return hash & hashmask(bits);
 }
 
-node *search_hash_table(node *hash_table[], char *find, unsigned find_len) {
+node *search_hash_table(node *hash_table[], char *find) {
+  int find_len = strlen(find);
   unsigned word_hash = oaat(find, find_len, NUM_BITS);
   node *wordptr = hash_table[word_hash];
   while (wordptr) {
@@ -85,29 +90,19 @@ int qcompare(const void* v1, const void* v2){
     return strcmp(n1->name, n2->name);
   return n2->score - n1->score;
 }
-//  qsort(parents, num_parents, sizeof(node*), qcompare);
 
-void printNodes(node **nodes, int len, char *header) {
-  printf("[DEBUG](%s) => ", header);
-  for (int i = 0; i < len; i++)
-    printf("[%d]%s(%d) ", i, nodes[i]->name, nodes[i]->score);
-  printf("\n");
+void score_tree(node **nodes, int num_nodes, int depth){
+  int pos;
+  for (pos = 0; pos < num_nodes; pos++)
+    nodes[pos]->score = count_descendants(nodes[pos], depth);
 }
 
-void solve_tree(node *hash_table[], node **parents, int num_parents,
-                int depth) {
-  int pos;
-  for (pos = 0; pos < num_parents; pos++)
-    parents[pos]->score = count_descendants(parents[pos], depth);
+void print_nodes(node **nodes, int num_nodes) {
 
-  // sort parents by parents->score: high -> low
-  // sort(parents, 0, num_parents, compare);
-  qsort(parents, num_parents, sizeof(node*), qcompare);
-
-  pos = 0;
-  int last_score = parents[pos]->score, cur_score, total = 0;
-  while (pos < num_parents) {
-    cur_score = parents[pos]->score;
+  int pos = 0;
+  int last_score = nodes[pos]->score, cur_score, total = 0;
+  while (pos < num_nodes) {
+    cur_score = nodes[pos]->score;
     // dont print those have zero descendant
     if (cur_score == 0)
       break;
@@ -116,95 +111,82 @@ void solve_tree(node *hash_table[], node **parents, int num_parents,
     if (cur_score < last_score && total == RANKING)
       break;
 
-    printf("%s %d\n", parents[pos]->name, cur_score);
+    printf("%s %d\n", nodes[pos]->name, cur_score);
     last_score = cur_score;
     total++;
     pos++;
   }
 }
 
-char *read_line(int size) {
-  char *str;
-  int ch;
-  int len = 0;
-  str = safe_malloc(size);
-  while ((ch = getchar()) != EOF && (ch != '\n')) {
-    str[len++] = ch;
-    /* len inc, check if size is too small */
-    if (len == size) {
-      size = size * 2;
-      str = realloc(str, size);
-      if (!str) {
-        fprintf(stderr, "malloc error\n");
-        exit(1);
+int read_tree(node **nodes, int num_lines) {
+  int n=0;
+  /* hash table to look up nodes */
+  node *hash_table[1 << NUM_BITS] = {NULL};
+  char *parent_name, *child_name;
+  node *parent, *child;
+  int num_children, hash;
+  for (int i=0; i < num_lines; i++){
+    // 1. read parent
+    parent_name = safe_malloc(MAXNAME +1);
+    scanf("%s", parent_name);
+    // 2. read num of children
+    scanf("%d", &num_children);
+
+    parent = search_hash_table(hash_table, parent_name);
+    if(!parent) {
+      // new parent and store it into hash_table
+      parent = new_node(parent_name);
+      hash = oaat(parent_name, strlen(parent_name), NUM_BITS);
+      parent->next = hash_table[hash];
+      hash_table[hash] = parent;
+      nodes[n++] = parent;
+    }
+    parent -> num_children = num_children;
+    parent->children = safe_malloc(sizeof(node*) * num_children);
+
+    // 3. read children
+    for (int j=0; j<num_children; j++){
+      child_name = safe_malloc(MAXNAME + 1);
+      scanf("%s", child_name);
+
+      child = search_hash_table(hash_table, child_name);
+      if(!child){
+        // new child and store it into hash_table
+        child = new_node(child_name);
+        hash = oaat(child_name, strlen(child_name), NUM_BITS);
+        child->next = hash_table[hash];
+        hash_table[hash] = child;
+        nodes[n++] = child;
       }
+      parent->children[j] = child;
     }
   }
-  str[len] = '\0';
-  return str;
+  return n;
 }
+
 
 int main(void) {
   int trees;
   scanf("%d", &trees);
-  getchar();
 
   for (int i = 0; i < trees; i++) {
-    /* hash table to look up nodes */
-    node *hash_table[1 << NUM_BITS] = {NULL};
-    int num_nodes, depth;
-    int pos = 0;
-    scanf("%d %d", &num_nodes, &depth);
-    getchar();
-    node **parents = safe_malloc(sizeof(node *) * num_nodes);
-    char *line;
-    for (int j = 1; j <= num_nodes; j++) {
-      /* read each line */
-      line = read_line(MAXNAME);
-      // split line into words by space
-      char *word = strtok(line, " ");
-      node *parent = search_hash_table(hash_table, word, strlen(word));
-      if (!parent) {
-        long parent_hash = oaat(word, strlen(word), NUM_BITS);
-        parent = new_node(word);
-        parent->next = hash_table[parent_hash];
-        hash_table[parent_hash] = parent;
-      }
-      // shoudl add to parents whether in hashtable or not
-      parents[pos++] = parent;
-
-      char *num_of_children = strtok(NULL, " ");
-      parent->num_children = atoi(num_of_children);
-      parent->children = safe_malloc(sizeof(node *) * parent->num_children);
-
-      // start reading children
-      node *child;
-      int c = 0;
-      word = strtok(NULL, " ");
-      while (word) {
-        child = search_hash_table(hash_table, word, strlen(word));
-        if (!child) {
-          // new child, add child to hash table
-          long child_hash = oaat(word, strlen(word), NUM_BITS);
-          child = new_node(word);
-          child->next = hash_table[child_hash];
-          hash_table[child_hash] = child;
-        }
-        // now child point to the correct child node
-        parent->children[c++] = child;
-        // process next word(child)
-        word = strtok(NULL, " ");
-      }
-    }
+    int num_lines, depth;
+    scanf("%d %d", &num_lines, &depth);
+    node **nodes = safe_malloc(sizeof(node *) * MAXNODES);
+    int num_nodes = read_tree(nodes, num_lines);
 
     printf("Tree %d:\n", i + 1);
 
-    // printNodes(parents, pos, "before");
-    solve_tree(hash_table, parents, pos, depth);
-    // printNodes(parents, pos, "final");
+    score_tree(nodes, num_nodes, depth);
+    qsort(nodes, num_nodes, sizeof(node*), qcompare);
+    print_nodes(nodes, num_nodes);
 
     if (i < trees)
       printf("\n");
+
+    // free nodes here
+    for (int j = 0; j < num_nodes; j++)
+      free(nodes[j]);
   }
   return 0;
 }
